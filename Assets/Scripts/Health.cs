6619f41578912d6427ca6ecd8afa70509b9e8090
Pomidorkin <pbriuzgin@gmail.com>
2021-05-18 16:33:21 +0900

    Implementing Health & Damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float damage = 50f;
 
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+
+        health.DealDamage(damage);
     }
 }
